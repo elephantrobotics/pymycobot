a100a7350a1a885dad63d3517758deeaa676c025
张立军 <zlj19971222@outlook.com>
2021-01-27 10:21:42 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
 import sys
 
-if (2, 7) != sys.version_info[:2] < (3, 5):
+
+PYTHON_VERSION = sys.version_info[:2]
+VERSION  = "2.1.2"
+
+if (2, 7) != PYTHON_VERSION < (3, 5):
     print("This mycobot version requires Python2.7, 3.5 or later.")
     sys.exit(1)
 
 import setuptools
 
-version  = "2.1.2"
 
-if sys.version_info[:2] == (2, 7):
+if PYTHON_VERSION == (2, 7):
     long_description = ""
 else:
     long_description = open("pymycobot/README.md").read()
+
+setuptools.setup(
+    name="pymycobot",
+    version=VERSION,
+    author="Zachary Zhang",
+    author_email="lijun.zhang@elephantrobotics.com",
+    description="Python API for serial communication of MyCobot.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elephantrobotics/pymycobot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pyserial'],
+    python_requires= '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, != 3.4.*',
+)
 
 change_log = '''
 # 2021.1.25
@@ -53,25 +80,4 @@ Python API supports python2.7
 Modify the serial port to manual setting, support the use of window.
 
 '''
-
-setuptools.setup(
-    name="pymycobot",
-    version=version,
-    author="Zachary Zhang",
-    author_email="lijun.zhang@elephantrobotics.com",
-    description="Python API for serial communication of MyCobot.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/elephantrobotics/pymycobot",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 2.7',
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=['pyserial'],
-    python_requires= '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, != 3.4.*',
-)
-
 
