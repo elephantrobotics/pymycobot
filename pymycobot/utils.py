35fec4a5b8363bbea99d60c6b0efd79e9e1b97ca
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-08-11 15:58:23 +0900

    remove redundancy

--- pymycobot/utils.py
+++ pymycobot/utils.py
@@ -1,22 +1,12 @@
-import re
-import serial.tools.list_ports
-
+from serial.tools.list_ports import comports
 
 def get_port_list():
-    plist = [str(x).split(" - ")[0].strip() for x in serial.tools.list_ports.comports()]
-    return plist
-
+    return [p.device for p in comports()]
 
 def detect_port_of_basic():
     """Detect M5 Basic.
     Returns the serial port string of the first detected M5 Basic.
     If it is not found, it returns `None`.
     """
-    type_re = re.compile(r"PID=[0-9a-zA-Z]+:([0-9a-zA-Z]+)")
-    plist = [x for x in serial.tools.list_ports.comports()]
-    for port in plist:
-        res = type_re.findall(port[2])
-        if res and res[0].lower() == "ea60":
-            return port[0]
-    else:
-        return None
+    ports = [p.device for p in comports() if p.pid == 0xea60]
+    return ports[0] if len(ports) > 0 else None
