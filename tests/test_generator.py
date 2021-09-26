2ff072841164148ffe054dcd037da649df4c66fa
nakano16180 <nakano16180@gmail.com>
2021-09-26 20:28:44 +0900

    add test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -10,85 +10,107 @@ from pymycobot import MyCobotCommandGenerator
 
 
 @pytest.fixture(scope="module")
-def setup():
-    global mg
+def mcg():
     print("")
     DEBUG = False
     mg = MyCobotCommandGenerator(debug=DEBUG)
     print("")
+    return mg
 
+def test_flatten1(mcg):
+    assert mcg._flatten([1,2,3,4,5,6]) == [1,2,3,4,5,6]
 
-def test_generator(setup):
+def test_flatten2(mcg):
+    assert mcg._flatten([1,2,3,4,[],5]) == [1,2,3,4,5]
+
+def test_flatten3(mcg):
+    assert mcg._flatten([1,2,3,4,[5,6],7]) == [1,2,3,4,5,6,7]
+
+def test_process_data_command(mcg):
+    assert mcg._process_data_command(()) == []
+
+def test_version(mcg):
+    assert mcg.version() == ([0xFE, 0xFE, 2, 0x00, 0xFA], True)
+
+def test_power_on(mcg):
+    assert mcg.power_on() == ([0xFE, 0xFE, 2, 0x10, 0xFA], False)
+
+def test_power_off(mcg):
+    assert mcg.power_off() == ([0xFE, 0xFE, 2, 0x11, 0xFA], False)
+
+def test_release_all_servos(mcg):
+    assert mcg.release_all_servos() == ([0xFE, 0xFE, 2, 0x13, 0xFA], False)
+
+def test_is_controller_connected(mcg):
+    assert mcg.is_controller_connected() == ([0xFE, 0xFE, 2, 0x14, 0xFA], True)
+
+def test_get_angles(mcg):
+    assert mcg.get_angles() == ([0xFE, 0xFE, 2, 0x20, 0xFA], True)
+
+def test_get_coords(mcg):
+    assert mcg.get_coords() == ([0xFE, 0xFE, 2, 0x23, 0xFA], True)
+
+def test_pause(mcg):
+    assert mcg.pause() == ([0xFE, 0xFE, 2, 0x26, 0xFA], False)
+
+def test_resume(mcg):
+    assert mcg.resume() == ([0xFE, 0xFE, 2, 0x28, 0xFA], False)
+
+def test_stop(mcg):
+    assert mcg.stop() == ([0xFE, 0xFE, 2, 0x29, 0xFA], False)
+
+def test_generator(mcg):
     print("")
-    pprint(mg.send_coords([-160, 160, 160, 0, 0, 0], 7, 2))
+    pprint(mcg.send_coords([-160, 160, 160, 0, 0, 0], 7, 2))
     print("")
-    pprint(mg.version())
+    pprint(mcg.send_angle(1, 0, 10))
     print("")
-    pprint(mg.power_on())
+    pprint(mcg.send_angles([0, 0, 0, 0, 0, 0], 5))
     print("")
-    pprint(mg.power_off())
-    print("")
-    pprint(mg.release_all_servos())
-    print("")
-    pprint(mg.is_controller_connected())
-    print("")
-    pprint(mg.get_angles())
-    print("")
-    pprint(mg.send_angle(1, 0, 10))
-    print("")
-    pprint(mg.send_angles([0, 0, 0, 0, 0, 0], 5))
-    print("")
-    pprint(mg.get_coords())
-    print("")
-    pprint(mg.send_coord(1, 110.5, 8))
-    print("pause")
-    pprint(mg.pause())
-    print("is_paused")
-    pprint(mg.is_paused())
-    print("resume")
-    pprint(mg.resume())
-    print("stop")
-    pprint(mg.stop())
+    pprint(mcg.send_coord(1, 110.5, 8))
+
     print("is_in_position")
-    pprint(mg.is_in_position([0, 0, 0, 0, 0, 0], 0))
+    pprint(mcg.is_in_position([0, 0, 0, 0, 0, 0], 0))
+
     print("")
-    pprint(mg.is_moving())
+    pprint(mcg.is_moving())
     print("")
-    pprint(mg.jog_angle(1, 0, 1))
+    pprint(mcg.jog_angle(1, 0, 1))
     print("")
-    pprint(mg.jog_coord(2, 1, 3))
+    pprint(mcg.jog_coord(2, 1, 3))
     print("")
-    pprint(mg.jog_stop())
+    pprint(mcg.jog_stop())
     print("set_encoder")
-    pprint(mg.set_encoder(1, 1024))
+    pprint(mcg.set_encoder(1, 1024))
+
     print("get_encoder")
-    pprint(mg.get_encoder(2))
-    print("get_speed")
-    pprint(mg.get_speed())
+    pprint(mcg.get_encoder(2))
     print("set_speed")
-    pprint(mg.set_speed(100))
+    pprint(mcg.set_speed(100))
+    print("get_speed")
+    pprint(mcg.get_speed())
     print("get_joint_min_angle")
-    pprint(mg.get_joint_min_angle(1))
+    pprint(mcg.get_joint_min_angle(1))
     print("get_joint_max_angle")
-    pprint(mg.get_joint_max_angle(2))
+    pprint(mcg.get_joint_max_angle(2))
     print("is_servo_enable")
-    pprint(mg.is_servo_enable(6))
+    pprint(mcg.is_servo_enable(6))
     print("is_all_servo_enable")
-    pprint(mg.is_all_servo_enable())
+    pprint(mcg.is_all_servo_enable())
     print("set_servo_data")
-    pprint(mg.set_servo_data(0, 1, 2))
+    pprint(mcg.set_servo_data(0, 1, 2))
     print("get_servo_data")
-    pprint(mg.get_servo_data(1, 2))
+    pprint(mcg.get_servo_data(1, 2))
     print("set_servo_calibration")
-    pprint(mg.set_servo_calibration(1))
+    pprint(mcg.set_servo_calibration(1))
     print("release_servo")
-    pprint(mg.release_servo(1))
+    pprint(mcg.release_servo(1))
     print("focus_servo")
-    pprint(mg.focus_servo(1))
+    pprint(mcg.focus_servo(1))
     print("set_color")
-    pprint(mg.set_color(255, 255, 0))
+    pprint(mcg.set_color(255, 255, 0))
     print("set_pin_mode")
-    pprint(mg.set_pin_mode(1, 0))
+    pprint(mcg.set_pin_mode(1, 0))
     print("set_digital_output")
-    pprint(mg.set_digital_output(0, 1))
-    pprint(mg)
+    pprint(mcg.set_digital_output(0, 1))
+    pprint(mcg)
