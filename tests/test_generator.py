a069e076804cc3e2978554bfd5c5842a5c9fed91
nakano16180 <nakano16180@gmail.com>
2021-10-04 07:14:18 +0900

    modified test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -26,8 +26,17 @@ def test_flatten2(mcg):
 def test_flatten3(mcg):
     assert mcg._flatten([1,2,3,4,[5,6],7]) == [1,2,3,4,5,6,7]
 
-def test_process_data_command(mcg):
+def test_encode_int16(mcg):
+    assert mcg._encode_int16([0, 1]) == sum([mcg._encode_int16(0), mcg._encode_int16(1)], [])
+
+def test_process_data_command1(mcg):
     assert mcg._process_data_command(()) == []
+
+def test_process_data_command2(mcg):
+    assert mcg._process_data_command((1, 2, 3)) == [1, 2, 3]
+
+def test_process_data_command3(mcg):
+    assert mcg._process_data_command((1, 2, [1, 2])) == mcg._flatten([1, 2, mcg._encode_int16([1, 2])])
 
 def test_version(mcg):
     assert mcg.version() == ([0xFE, 0xFE, 2, 0x00, 0xFA], True)
@@ -50,6 +59,9 @@ def test_get_angles(mcg):
 def test_get_coords(mcg):
     assert mcg.get_coords() == ([0xFE, 0xFE, 2, 0x23, 0xFA], True)
 
+def test_is_in_position(mcg):
+    assert mcg.is_in_position([0, 0, 0, 0, 0, 0], 0) == (mcg._flatten([0xFE, 0xFE, 15, 0x2A, mcg._flatten([mcg._encode_int16([0, 0, 0, 0, 0, 0]), 0]), 0xFA]), True)
+
 def test_pause(mcg):
     assert mcg.pause() == ([0xFE, 0xFE, 2, 0x26, 0xFA], False)
 
@@ -59,58 +71,8 @@ def test_resume(mcg):
 def test_stop(mcg):
     assert mcg.stop() == ([0xFE, 0xFE, 2, 0x29, 0xFA], False)
 
-def test_generator(mcg):
-    print("")
-    pprint(mcg.send_coords([-160, 160, 160, 0, 0, 0], 7, 2))
-    print("")
-    pprint(mcg.send_angle(1, 0, 10))
-    print("")
-    pprint(mcg.send_angles([0, 0, 0, 0, 0, 0], 5))
-    print("")
-    pprint(mcg.send_coord(1, 110.5, 8))
+def test_set_pin_mode(mcg):
+    assert mcg.set_pin_mode(1, 0) == ([0xFE, 0xFE, 4, 0x60, 1, 0, 0xFA], False)
 
-    print("is_in_position")
-    pprint(mcg.is_in_position([0, 0, 0, 0, 0, 0], 0))
-
-    print("")
-    pprint(mcg.is_moving())
-    print("")
-    pprint(mcg.jog_angle(1, 0, 1))
-    print("")
-    pprint(mcg.jog_coord(2, 1, 3))
-    print("")
-    pprint(mcg.jog_stop())
-    print("set_encoder")
-    pprint(mcg.set_encoder(1, 1024))
-
-    print("get_encoder")
-    pprint(mcg.get_encoder(2))
-    print("set_speed")
-    pprint(mcg.set_speed(100))
-    print("get_speed")
-    pprint(mcg.get_speed())
-    print("get_joint_min_angle")
-    pprint(mcg.get_joint_min_angle(1))
-    print("get_joint_max_angle")
-    pprint(mcg.get_joint_max_angle(2))
-    print("is_servo_enable")
-    pprint(mcg.is_servo_enable(6))
-    print("is_all_servo_enable")
-    pprint(mcg.is_all_servo_enable())
-    print("set_servo_data")
-    pprint(mcg.set_servo_data(0, 1, 2))
-    print("get_servo_data")
-    pprint(mcg.get_servo_data(1, 2))
-    print("set_servo_calibration")
-    pprint(mcg.set_servo_calibration(1))
-    print("release_servo")
-    pprint(mcg.release_servo(1))
-    print("focus_servo")
-    pprint(mcg.focus_servo(1))
-    print("set_color")
-    pprint(mcg.set_color(255, 255, 0))
-    print("set_pin_mode")
-    pprint(mcg.set_pin_mode(1, 0))
-    print("set_digital_output")
-    pprint(mcg.set_digital_output(0, 1))
-    pprint(mcg)
+def test_set_digital_output(mcg):
+    assert mcg.set_digital_output(0, 1) == ([0xFE, 0xFE, 4, 0x61, 0, 1, 0xFA], False)
