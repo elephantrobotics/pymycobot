2095d3f8ea60f92bcf5d041ec305f588a024cfed
weiquan <weiquan.xu@elephantrobotics.com>
2022-08-01 15:47:46 +0800

    release v2.9.1

--- pymycobot/__init__.py
+++ pymycobot/__init__.py
@@ -30,7 +30,7 @@ __all__ = [
     "Mira"
 ]
 
-__version__ = "2.9.0"
+__version__ = "2.9.1"
 __author__ = "Elephantrobotics"
 __email__ = "weiquan.xu@elephantrobotics.com"
 __git_url__ = "https://github.com/elephantrobotics/pymycobot"
