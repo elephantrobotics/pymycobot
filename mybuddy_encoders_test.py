a869a7232aa3a113b2ccc326b3e6e25a07aa32be
wxy6655 <hongyang.wang@elephantrobotics.com>
2022-09-06 18:42:20 +0800

    update mybuddy test

--- mybuddy_encoders_test.py
+++ mybuddy_encoders_test.py
@@ -1,25 +1,34 @@
+#import library
 import time
 import os
 import sys
 import serial
 import serial.tools.list_ports
-
+import platform
 from pymycobot.mybuddy import MyBuddy
 
-
+#define type
 port: str
 mc: MyBuddy
-port = 'COM21'
-baud = 115200
 DEBUG = False
 
-encoders = [2047,2047,2047,2047,2047,2047,2047,2047,2047,2047,2047,2047,2047,2047,2047]
-speeds = [500,500,500,500,500,500,500,500,500,500,500,500,500,500,500]
+# auto select system port
+if platform.system() == 'Windows':
+    port = 'COM21'
+    baud = 115200
+elif platform.system() == 'Linux':
+    port = '/dev/ttyACM0'
+    baud = 115200
+    
+# demo encoders and speeds
+encoders = [2047,2047,2047,2047,2047,2047,2047, 2047,2047,2047,2047,2047,2047,2047, 2047]        # Lram + gipper  &  Rram + gipper & Wram  encoders
+speeds = [500,500,500,500,500,500,500, 500,500,500,500,500,500,500, 500]                         # Lram + gipper  &  Rram + gipper & Wram  speeds
 
+# Connect Robot
 mc = MyBuddy(port, baud, debug=DEBUG)
-# print(mc.get_angles(1))
+
+# print all encoders and speeds
 print(mc.get_encoders(0))
 
+# set run demo encoders，init robot
 mc.set_encoders(0,encoders,speeds)
-
-# mc.set_encoders(1,[1,1,1,1,1,1],[0,2,0,2,0,2])
