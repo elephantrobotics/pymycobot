551b87640305b15d09527491a43de0644814bec6
weiquan <weiquan.xu@elephantrobotics.com>
2022-10-12 14:38:31 +0800

    push code

--- pymycobot/mybuddyemoticon.py
+++ pymycobot/mybuddyemoticon.py
@@ -7,27 +7,33 @@ import threading
 
 
 class Emoticon:
-    def __init__(self, file_path: list = [], window_size: list = [], play_time=5) -> None:
+    def __init__(self, file_path: list = [], window_size: list = []) -> None:
         """API for playing emoticons
 
         Args:
-            file_path(list): Absolute path of facial expression video.
+            file_path(list): `[[path, time]]`The absolute path of facial expression video and the length of time to play.Time in seconds.
             window_size(list): `[Length, width] `Size of the playback window (default is full screen).
-            play_time: Playback duration. 5 seconds by default
 
         """
         self.__file_path = file_path
         self.__window_size = window_size
-        self.__play_time = play_time
         self.quit = False
         self.stop_play = False
         
     @property
     def file_path(self):
+        """Get Playfile List"""
         return self.__file_path
     
-    def add_file_path(self, path):
-        self.__file_path.append(path)
+    def add_file_path(self, path_time: list):
+        """Add Playback File"""
+        self.__file_path.append(path_time)
+        
+    def del_file_path(self, index: int):
+        """Delete the element with the specified subscript in the playlist list"""
+        if index >= len(self.__file_path):
+            raise IndexError("list index out of range")
+        self.__file_path.pop(index)
 
     @property
     def window_size(self):
@@ -35,33 +41,25 @@ class Emoticon:
     
     @window_size.setter
     def window_size(self, data):
+        """Set playback window size"""
         self.__window_size = data
-
-    @property
-    def play_time(self):
-        return self.__play_time
-    
-    @play_time.setter
-    def play_time(self, data):
-        self.__play_time = data
         
     def mouse_callback(self, event, x, y, flags, param):
+        """Mouse click callback function"""
         if event == cv.EVENT_LBUTTONDOWN:
             self.quit = True
             
-    def stop(self):
+    def pause(self):
+        """Pause playback"""
         self.stop_play = True
         
-    def start(self):
-        self.stop_play = False
-        
     def run(self):
-        t = threading.Thread(target=self.play, daemon=True)
-        t.start()
+        """Continue playing"""
+        self.stop_play = False
 
     def play(self):
         index = 0
-        cap = cv.VideoCapture(self.__file_path[index])
+        cap = cv.VideoCapture(self.__file_path[index][0])
         out_win = "frame"
         cv.namedWindow(out_win, cv.WINDOW_NORMAL)
         if not self.window_size:
@@ -70,17 +68,32 @@ class Emoticon:
             cv.resizeWindow(out_win, self.window_size[0], self.window_size[1])
         t = time.time()
         cv.setMouseCallback(out_win, self.mouse_callback)
-        while time.time() - t < self.play_time and self.quit == False:
-            while self.stop_play:
-                pass
-            ret, frame = cap.read()
-            # print(frame)
-            if frame is not None:
-                cv.imshow(out_win, frame)
-            if cv.waitKey(1) & 0xFF == ord('q') or ret == False:
-                index += 1
-                if index >= len(self.__file_path):
-                    index = 0
-                cap = cv.VideoCapture(self.__file_path[index])
+        while True:
+            while time.time() - t < self.__file_path[index][1]:
+                if self.quit:
+                    break
+                while self.stop_play and self.quit == False:
+                    pass
+                ret, frame = cap.read()
+                # print(frame)
+                if frame is not None:
+                    cv.imshow(out_win, frame)
+                if cv.waitKey(1) & 0xFF == ord('q') or ret == False:
+                    if time.time() - t >= self.__file_path[index][1]:
+                        index += 1
+                        if index >= len(self.__file_path):
+                            index = 0
+                    cap = cv.VideoCapture(self.__file_path[index][0])
+            if self.quit:
+                break
         cap.release()
         cv.destroyAllWindows()
+        
+    def start(self):
+        """Start playing"""
+        self.t = threading.Thread(target=self.play, daemon=True)
+        self.t.start()
+        
+    def joint(self):
+        """Wait for the thread playing the video to finish"""
+        self.t.joint()
