fdd7a53e7abd641ce3ff86617b954a6825cd4b8f
Leonid V. Fedorenchik <leonid@fedorenchik.com>
2024-04-29 11:15:40 +0800

    Fix tool_get_firmware_version

--- pymycobot/mycobotpro630.py
+++ pymycobot/mycobotpro630.py
@@ -2572,10 +2572,12 @@ class Phoenix:
         Returns:
             float: firmware version
         """
+        version = 0.0
         self.send_can(data=[0x01, 0x09])
-        msg = self.receive_can()
-        print("msg.data = " + str(msg.data))
-        version = msg.data[2] / 10
+        for _ in range(100):
+            msg = self.receive_can()
+            if msg.data[0] == 0x02 and msg.data[1] == 0x09:
+                version = msg.data[2] / 10
         return version
 
     def tool_set_digital_out(self, pin_no, pin_value):
