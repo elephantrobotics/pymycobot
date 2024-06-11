6c06272b2826e7c25c0ad1c59b58a56e6fdc885c
weiquan <weiquan.xu@elephantrobotics.com>
2024-06-11 16:26:46 +0800

    save code

--- pymycobot/mycobot.py
+++ pymycobot/mycobot.py
@@ -164,6 +164,17 @@ class MyCobot(CommandGenerator, PublicCommandGenerator):
                     else:
                         r.append(self._int2angle(res[index]))
                 return r
+            elif genre == ProtocolCode.MERCURY_ROBOT_STATUS:
+                for i in range(len(res)):
+                    if res[i] != 0:
+                        data = bin(res[i])[2:]
+                        res[i] = []
+                        while len(data) != 16:
+                            data = "0"+data
+                        for j in range(16):
+                            if data[j] != "0":
+                                res[i].append(15-j)
+                return res
             else:
                 return res
         return None
--- pymycobot/mycobotsocket.py
+++ pymycobot/mycobotsocket.py
@@ -153,6 +153,17 @@ class MyCobotSocket(CommandGenerator):
                         else:
                             r.append(self._int2angle(res[index]))
                     return r
+                elif genre == ProtocolCode.MERCURY_ROBOT_STATUS:
+                    for i in range(len(res)):
+                        if res[i] != 0:
+                            data = bin(res[i])[2:]
+                            res[i] = []
+                            while len(data) != 16:
+                                data = "0"+data
+                            for j in range(16):
+                                if data[j] != "0":
+                                    res[i].append(15-j)
+                    return res
                 else:
                     return res
             return None
