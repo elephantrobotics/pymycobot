00735319e12222b6d59955107288706391a9d20c
wangWking <842749351@qq.com>
2025-08-26 15:33:38 +0800

    fix(320 Api):Update the increment range limit of angle stepping and coordinate stepping interfaces

--- pymycobot/mycobot320.py
+++ pymycobot/mycobot320.py
@@ -365,10 +365,13 @@ class MyCobot320(CommandGenerator):
         Args:
             joint_id: int 1-6.
             increment: Angle increment value
-            speed: int (0 - 100)
+            speed: int (1 - 100)
         """
+
         self.calibration_parameters(class_name=self.__class__.__name__, id=joint_id, increment_angle=increment, speed=speed)
-        return self._mesg(ProtocolCode.JOG_INCREMENT, joint_id, [self._angle2int(increment)], speed)
+        scaled_increment = self._angle2int(increment)
+        scaled_increment = max(min(scaled_increment, 32767), -32768)
+        return self._mesg(ProtocolCode.JOG_INCREMENT, joint_id, [scaled_increment], speed)
 
     def jog_increment_coord(self, id, increment, speed):
         """coord step mode
@@ -379,7 +382,11 @@ class MyCobot320(CommandGenerator):
             speed: int (1 - 100)
         """
         self.calibration_parameters(class_name=self.__class__.__name__, id=id, increment_coord=increment, speed=speed)
-        value = self._coord2int(increment) if id <= 3 else self._angle2int(increment)
+        if id <= 3:
+            value = self._coord2int(increment)
+        else:
+            scaled_increment = self._angle2int(increment)
+            value = max(min(scaled_increment, 32767), -32768)
         return self._mesg(ProtocolCode.JOG_INCREMENT_COORD, id, [value], speed)
 
     # Basic for raspberry pi.
