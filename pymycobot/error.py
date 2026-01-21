8a7b02fcabf67954ea0328d33b503f7211af69f7
wangWking <842749351@qq.com>
2026-01-21 09:31:22 +0800

    perf(450-api): update pro gripper offset api range

--- pymycobot/error.py
+++ pymycobot/error.py
@@ -1859,8 +1859,8 @@ def calibration_parameters(**kwargs):
                         f"motor_type {hex(value)} not allowed. Must be one of: {[hex(x) for x in allowed_types]}")
             elif parameter in ["gripper_offset"]:
                 check_value_type(parameter, value_type, MyCobotPro450DataException, int)
-                if value < -127 or value > 127:
-                    raise MyCobotPro450DataException("The parameter {} only supports -127 ~ 127, but received {}".format(parameter, value))
+                if value < -5 or value > 5:
+                    raise MyCobotPro450DataException("The parameter {} only supports -5 ~ 5, but received {}".format(parameter, value))
 
     elif class_name in ["ultraArmP340"]:
         for parameter in parameter_list[1:]:
