a9f6f872c419d7f8986a3d77926bcfdada37094c
jiaweilong66 <627892495@qq.com>
2026-07-24 16:50:00 +0800

    delete some nousing api

--- pymycobot/myagvplussocket.py
+++ pymycobot/myagvplussocket.py
@@ -774,8 +774,6 @@ class MyAGVPlusSocket(object):
         Returns:
             int: firmware response, or -1 if failed.
         """
-        if state not in (0, 1):
-            raise ValueError("Invalid state. Range must be 0 or 1")
         self.calibration_parameters(class_name=self.__class__.__name__, state=state)
         return self._rpc_call("set_pump_state", state)
 
@@ -791,8 +789,6 @@ class MyAGVPlusSocket(object):
         """
         if pin not in (2, 5):
             raise ValueError("Invalid pin. Range must be 2 or 5")
-        if state not in (0, 1):
-            raise ValueError("Invalid state. Range must be 0 or 1")
         self.calibration_parameters(class_name=self.__class__.__name__, pin=pin, state=state)
         return self._rpc_call("set_pump_io", pin, state)
 
